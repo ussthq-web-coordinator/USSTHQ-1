386da263e70799c4931e2bda44dbb34ce0f90288
ussthq-web-coordinator <web.coordinator@uss.salvationarmy.org>
2025-05-23 00:15:40 -0400

    Gallery Latest to main

--- commissioning-2025-loader.js
+++ commissioning-2025-loader.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/ussthq-web-coordinator/USSTHQ-1@latest/commissioning-2025-gallery.js";
+  script.src = "https://cdn.jsdelivr.net/gh/ussthq-web-coordinator/USSTHQ-1@main/commissioning-2025-gallery.js";
   script.type = "text/javascript";
   script.defer = true;
   document.body.appendChild(script);
