293382e8b82ce68574cd3d4f60db02ad6e610f49
ussthq-web-coordinator <web.coordinator@uss.salvationarmy.org>
2025-09-22 18:02:21 -0400

    Refreshed Data

--- Site-Migration-Dashboard.js
+++ Site-Migration-Dashboard.js
@@ -31,7 +31,7 @@
 })();
 
 
-const jsonURL = "https://cdn.jsdelivr.net/gh/ussthq-web-coordinator/USSTHQ-1@latest/DashboardData-m.json";
+const jsonURL = "https://cdn.jsdelivr.net/gh/ussthq-web-coordinator/USSTHQ-1@latest/DashboardData-n.json";
 let table, tableData=[], pageCache={}, qaGroupedCache={};
 let priorityChart, pageTypeChart, pubSymChart;
 let statusChart = null;
