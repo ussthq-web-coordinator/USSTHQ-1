96ff9920faedc33bee946385878aca4008b21398
ussthq-web-coordinator <web.coordinator@uss.salvationarmy.org>
2025-10-13 13:24:18 -0400

    Corrected Spelling of Arkansas URL

--- volunteermapdata-south.js
+++ volunteermapdata-south.js
@@ -166,7 +166,7 @@ var simplemaps_usmap_mapdata={
     AR: {
       name: "Arkansas",
       color: "#999999",
-      url: "https://www.salvationarmyusa.org/usa-southern-territory/arkanasas-and-oklahoma/contact-us#contact",
+      url: "https://www.salvationarmyusa.org/usa-southern-territory/arkansas-and-oklahoma/contact-us#contact",
       popup: "on_click" 
     },
     MO: {
