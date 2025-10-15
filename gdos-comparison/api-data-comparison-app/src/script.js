f0ccbb62f650c91858f338f9867b88f859095127
ussthq-web-coordinator <web.coordinator@uss.salvationarmy.org>
2025-10-15 03:18:47 -0400

    Fixed Fetching

--- gdos-comparison/api-data-comparison-app/src/script.js
+++ gdos-comparison/api-data-comparison-app/src/script.js
@@ -12,7 +12,7 @@ Promise.all([
         if (!response.ok) throw new Error('USS file not found');
         return response.json();
     }),
-    fetch('../LocationsData.json').then(response => {
+    fetch('../../../LocationsData.json').then(response => {
         if (!response.ok) throw new Error('LocationsData file not found');
         return response.json();
     })
